aecdd7af9b25fd8d4a25a4fafd1e76dd27d430d2
Pleshyvtsev-Mykhailo <pleshyvtsev@knu.ua>
2026-05-25 04:10:26 +0300

    Add random mine generation to Board

--- main.py
+++ main.py
@@ -29,3 +29,15 @@ class Board:
         self.num_mines = num_mines
         self.grid = [[Cell(x, y) for x in range(cols)] for y in range(rows)]
         self.mines_generated = False
+    
+    def generate_mines(self, first_click_x, first_click_y):
+        mines_placed = 0
+        while mines_placed < self.num_mines:
+            x = random.randint(0, self.cols - 1)
+            y = random.randint(0, self.rows - 1)
+
+            if not self.grid[y][x].is_mine and (x != first_click_x or y != first_click_y):
+                self.grid[y][x].is_mine = True
+                mines_placed += 1
+        self.calculate_neighbors()
+        self.mines_generated = True
